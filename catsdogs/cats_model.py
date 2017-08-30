dfadd0ec98d3d1bbaf89b74a26278ad562bf390b
chenwu <chengyi.wu@outlook.com>
2017-08-30 15:43:00 +0800

    + bcolz

--- catsdogs/cats_model.py
+++ catsdogs/cats_model.py
@@ -12,7 +12,7 @@ class CATSModel(Model):
         self.build()
 
     def add_placeholders(self):
-        self.input_placeholder = tf.placeholder(dtype=tf.float32, shape=(None, 32, 32, 3), name='X')
+        self.input_placeholder = tf.placeholder(dtype=tf.float32, shape=(None, 64, 64, 3), name='X')
         self.labels_placeholder = tf.placeholder(dtype=tf.int64, shape=(None), name='y')
 
     def create_feed_dict(self, inputs_batch, labels_batch=None):
@@ -32,30 +32,62 @@ class CATSModel(Model):
             num_outputs=64,
             kernel_size=3,
             stride=1,
-            activation_fn=None
         )
-        h = tf.nn.relu(a)
-        h = tf.contrib.layers.max_pool2d(
-            inputs=h,
-            kernel_size=2,
-            stride=2,
-        )
-
         a = tf.contrib.layers.conv2d(
-            inputs=h,
+            inputs=a,
             num_outputs=64,
             kernel_size=3,
             stride=1,
-            activation_fn=None
         )
-        h = tf.nn.relu(a)
         h = tf.contrib.layers.max_pool2d(
-            inputs=h,
+            inputs=a,
             kernel_size=2,
             stride=2,
+        ) # 16 * 16 * 64
+
+        a = tf.contrib.layers.conv2d(
+            inputs=h,
+            num_outputs=128,
+            kernel_size=3,
+            stride=1,
         )
+        a = tf.contrib.layers.conv2d(
+            inputs=a,
+            num_outputs=128,
+            kernel_size=3,
+            stride=1,
+        )
+        h = tf.contrib.layers.max_pool2d(
+            inputs=a,
+            kernel_size=2,
+            stride=2,
+        ) # 8 * 8 * 128
+
+        a = tf.contrib.layers.conv2d(
+            inputs=h,
+            num_outputs=256,
+            kernel_size=3,
+            stride=1,
+        )
+        a = tf.contrib.layers.conv2d(
+            inputs=a,
+            num_outputs=256,
+            kernel_size=3,
+            stride=1,
+        )
+        a = tf.contrib.layers.conv2d(
+            inputs=a,
+            num_outputs=256,
+            kernel_size=3,
+            stride=1,
+        )
+        h = tf.contrib.layers.max_pool2d(
+            inputs=a,
+            kernel_size=2,
+            stride=2,
+        ) # 4 * 4 * 256
         
-        h_flat = tf.reshape(h, shape=(-1, 8 * 8 * 64))
+        h_flat = tf.reshape(h, shape=(-1, 8 * 8 * 256))
 
         y_out = tf.contrib.layers.fully_connected(
             inputs=h_flat,
@@ -94,29 +126,27 @@ class CATSModel(Model):
         )
         return loss, corr
 
-    def run_epoch(self, sess, batch_size, training_set, is_training=True):
+    def run_epoch(self, sess, batch_size, training_set, validation_set):
         X_tr, Y_tr = training_set
+        X_val, Y_val = validation_set
 
-        prog = Progbar(target=1 + X_tr.shape[0] // batch_size)
-        
-        total_correct, total_items = 0, 0
+        prog = Progbar(target=X_tr.shape[0] // batch_size)
 
-        for i, (train_x, train_y) in enumerate(get_minibatches(X_tr, Y_tr, batch_size)):
-            
-            loss, corr = self.train_on_batch(sess, train_x, train_y, is_training)
-            total_correct += np.sum(corr)
-            total_items += train_y.shape[0]
-            if is_training:
-                prog.update(i + 1, [('train loss', loss)], [('train accuracy', total_correct / total_items)])
-            else:
-                prog.update(i + 1, None, [('validation accuracy', total_correct / total_items)])
+        for i, (train_x, train_y) in enumerate(get_minibatches(X_tr, Y_tr, batch_size)):     
+            loss, corr = self.train_on_batch(sess, train_x, train_y, True)
+            prog.update(i + 1, [('train loss', loss), ('train_acc', np.sum(corr) / train_x.shape[0])])
 
+        val_loss, val_corr = 0, 0
+        for (val_x, val_y) in get_minibatches(X_val, Y_val, batch_size, False):
+            loss, corr = self.train_on_batch(sess, val_x, val_y, False)
+            val_loss += loss
+            val_corr += np.sum(corr)
+        print("Validation loss = {0:.3g} and accuracy = {1:.3g}".format(val_loss, val_corr / X_val.shape[0]))
         
     def fit(self, sess, epoches, batch_size, training_set, validation_set):
         for epoch in range(epoches):
-            print("\nEpch {:} out of {:}".format(epoch + 1, epoches))
-            self.run_epoch(sess, batch_size, training_set)
-            self.run_epoch(sess, batch_size, validation_set, is_training=False)
+            print("\nEpoch {:} out of {:}".format(epoch + 1, epoches))
+            self.run_epoch(sess, batch_size, training_set, validation_set)
 
 def get_minibatches(data, labels, minibatch_size, shuffle=True):
     data_size = data.shape[0]
@@ -128,7 +158,7 @@ def get_minibatches(data, labels, minibatch_size, shuffle=True):
         yield data[idx], labels[idx]
 
 def main(debug=True):
-    X_tr, Y_tr, X_val, Y_val, X_te = load_data("datasets")
+    X_tr, Y_tr, X_val, Y_val, X_te = load_data("datasets", resize=(64, 64))
 
     tf.reset_default_graph()
 
@@ -139,7 +169,7 @@ def main(debug=True):
             sess.run(tf.global_variables_initializer())
             model.fit(
                 sess,
-                1,
+                5,
                 32,
                 (X_tr, Y_tr),
                 (X_val, Y_val)
--- catsdogs/data_utils.py
+++ catsdogs/data_utils.py
@@ -1,5 +1,5 @@
 import numpy as np
-import platform, os, random
+import platform, os, random,bcolz
 from PIL import Image
 
 def one_hot_encoding(filename):
@@ -9,12 +9,46 @@ def one_hot_encoding(filename):
     else:
         return 0
 
+def save_array(data_folder, fname, arr):
+    fname = os.path.join(data_folder, fname)
+    print("Saving to {0} ...".format(fname))
+    c = bcolz.carray(arr, rootdir=fname, mode='w')
+    c.flush()
+
+def load_array(data_folder, fname):
+    fname = os.path.join(data_folder, fname)
+    print("Loading from {0} ...".format(fname))
+    return bcolz.open(fname)[:]
+
+def load_from_cache(data_folder, size):
+    X_tr = load_array(data_folder, 'train{0}_data.npz'.format(size))
+    Y_tr = load_array(data_folder, 'train{0}_label.npz'.format(size))
+    X_val = load_array(data_folder, 'val{0}_data.npz'.format(size))
+    Y_val = load_array(data_folder, 'val{0}_label.npz'.format(size))
+
+    X_te = load_array(data_folder, 'test{0}.npz'.format(size))
+
+    return X_tr, Y_tr, X_val, Y_val, X_te
+
+def save_to_cache(data_folder, size, X_tr, Y_tr, X_val, Y_val, X_te):
+    save_array(data_folder, 'train{0}_data.npz'.format(size), X_tr)
+    save_array(data_folder, 'train{0}_label.npz'.format(size), Y_tr)
+    save_array(data_folder, 'val{0}_data.npz'.format(size), X_val)
+    save_array(data_folder, 'val{0}_label.npz'.format(size), Y_val)
+    save_array(data_folder, 'test{0}.npz'.format(size), X_te)
+
 def load_data(data_folder, num_training_ratio=0.8, resize=(32, 32)):
     '''
     load all of cats/dogs
     '''
+    try:
+        return load_from_cache(data_folder, size=resize[0])
+    except FileNotFoundError:
+        print("Cache not found, loading from raw images...")
+        pass
+
     training_folder = os.path.join(data_folder, "train")
-    testing_folder = os.path.join(data_folder, "test")  
+    testing_folder = os.path.join(data_folder, "test")
 
     X = []
     Y = []
@@ -48,7 +82,9 @@ def load_data(data_folder, num_training_ratio=0.8, resize=(32, 32)):
 
     X_te -= mean_image
 
+    save_to_cache(data_folder, resize[0],  X_tr, Y_tr, X_val, Y_val, X_te)
+
     return X_tr, Y_tr, X_val, Y_val, X_te
 
 if __name__ == '__main__':
-    load_data('datasets')
+    load_data('datasets', resize=(64,64))
